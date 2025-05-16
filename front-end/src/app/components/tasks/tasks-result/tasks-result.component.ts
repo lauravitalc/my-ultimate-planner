f9387d3e5f3e181faad533280877d1a34b4a465b
lauravitalc <laura_vitalc@hotmail.com>
2025-05-16 20:23:19 -0300

    feat: add checkbox handling methods WIP

--- front-end/src/app/components/tasks/tasks-result/tasks-result.component.ts
+++ front-end/src/app/components/tasks/tasks-result/tasks-result.component.ts
@@ -12,15 +12,33 @@ export interface Task {
   styleUrl: './tasks-result.component.scss',
 })
 export class TasksResultComponent {
-  displayedColumns: string[] = ['select','name', 'status', 'actions'];
-  dataSource: Task[] = [{
-    taskName: 'Task 1',
-    status: 'In Progress',
-    actions: 'Edit'
-  },
-  {
-    taskName: 'Task 2',
-    status: 'In Progress',
-    actions: 'Edit'
-  }]
+  displayedColumns: string[] = ['select', 'name', 'status', 'actions'];
+
+  dataSource: Task[] = [
+    {
+      taskName: 'Task 1',
+      status: 'In Progress',
+      actions: 'Edit',
+    },
+    {
+      taskName: 'Task 2',
+      status: 'In Progress',
+      actions: 'Edit',
+    },
+  ];
+
+  selection: Task[] = [];
+
+  checkAllRows(event: any) {
+    console.log(event);
+    if (event.checked) {
+      this.selection = [...this.dataSource];
+    } else {
+      this.selection = [];
+    }
+  }
+
+  checkRow(row: Task) {
+    console.log(row);
+  }
 }
