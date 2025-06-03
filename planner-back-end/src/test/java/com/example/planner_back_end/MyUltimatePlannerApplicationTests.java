2aeb10a093edf4cbf9d3ab90465c2947e430cb06
lauravitalc <laura_vitalc@hotmail.com>
2025-06-03 19:55:16 -0300

    feat: Spring initilzr files

---
+++ planner-back-end/src/test/java/com/example/planner_back_end/MyUltimatePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planner_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUltimatePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
